fb8cc7a79afc6747983b0401df1c4f8c1f0e3447
jan-beszterda <jan.beszterda@gmail.com>
2021-10-03 09:05:16 +0200

    Updated comment in Main class.

--- src/stensaxpase/Main.java
+++ src/stensaxpase/Main.java
@@ -1,7 +1,7 @@
 package stensaxpase;
 
 /**
- * Main class responsible for: creating the Game object and starting the program.
+ * Main class responsible for creating the Game object and starting the program.
  * @author Jan Beszterda
  */
 public class Main {
